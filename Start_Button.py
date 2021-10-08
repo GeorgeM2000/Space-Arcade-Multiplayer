5390ba58fdd33480960a75e6471e06bfc106d79b
George M <giorgos_matlis@hotmail.com>
2021-10-08 23:06:04 +0300

    Create Start_Button.py

---
+++ Start_Button.py
@@ -0,0 +1,31 @@
+import pygame.font
+
+class StartButton():
+
+    def __init__(self, screen, message):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        self.running_state = False
+
+        self.width, self.height = 200, 50
+        self.button_color = (0,255,0)
+        self.text_color = (255,255,255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        self.prep_message(message)
+
+    def prep_message(self, message):
+        self.msg_image = self.font.render(message, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    
+    def draw_button(self, running_state):
+        if not running_state:
+            self.screen.fill(self.button_color, self.rect)
+            self.screen.blit(self.msg_image, self.msg_image_rect)
+
