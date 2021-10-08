a96c55220832e5fd8aa9008ed5a5fa8b3364bad3
George M <giorgos_matlis@hotmail.com>
2021-10-08 23:06:07 +0300

    Create Spaceship_First_Player.py

---
+++ Spaceship_First_Player.py
@@ -0,0 +1,37 @@
+import pygame as pg
+
+class Spaceship_First_Player():
+
+    def __init__(self, screen):
+        self.screen = screen
+
+        self.image = pg.image.load('C:\\Users\\giorg\\SpaceshipArcade\\ship.bmp')
+
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.right_movement = False
+        self.left_movement = False
+
+        self.speed_factor = None
+        self.bullets_allowed = 3
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.life_reduction = 1
+        self.initial_life = 100
+
+    def update(self):
+        if self.right_movement and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += self.speed_factor
+        elif self.left_movement and self.rect.left > 0:
+            self.rect.centerx -= self.speed_factor
+
+    def blitme(self):
+        self.screen.blit(pg.transform.scale(self.image, (60,48)), self.rect)
+
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
