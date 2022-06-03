310ee0d915242a74639728a46d925818ede740ce
GeorgeM2000 <giorgos_matlis@hotmail.com>
2022-06-03 08:51:14 +0300

    Commit Message

--- Menu.py
+++ Menu.py
@@ -19,7 +19,7 @@ class Menu_Button():
 			self.image = self.text
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
+ 
 	def update(self, screen):
 		if self.image is not None:
 			screen.blit(self.image, self.rect)
